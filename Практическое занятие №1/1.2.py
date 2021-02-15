336179c2861a33ae1b2ff9bf49b5368dea3aa38a
Truong Son <tr.son.2112@gmail.com>
2021-02-15 15:38:53 +0700

    fix bug

--- Практическое занятие №1/1.2.py
+++ Практическое занятие №1/1.2.py
@@ -1,12 +1,9 @@
 import math
 
-def f(x):
+def f12(x):
     if x < 56:
-        return math.sin(math.exp(x)) + math.sin(x) - 34
+        return "{:.2e}".format(math.sin(math.exp(x)) + math.sin(x) - 34)
     elif x >= 56 and x < 91:
-        return 21*(x**3) - math.cos(x)
+        return "{:.2e}".format(21*(x**3) - math.cos(x))
     else:
-        return (x**3 + x/5 - 52)**4 - math.tan(x)
-
-print("f(59) = {:.2E}".format(f(59)))
-print("f(86) = {:.2E}".format(f(86)))
+        return "{:.2e}".format((x**3 + x/5 - 52)**4 - math.tan(x))
--- Практическое занятие №1/1.4.py
+++ Практическое занятие №1/1.4.py
@@ -1,10 +1,7 @@
 import math
 
-def f(n):
+def f14(n):
     if n == 0:
         return 6
     else:
-        return math.cos(f(n-1)) - math.sin(f(n-1))
-
-print("f(3) = {:.2E}".format(f(3)))
-print("f(9) = {:.2E}".format(f(9)))
+        return "{:.2e}".format(math.cos(float(f14(n-1))) - math.sin(float(f14(n-1))))
